a5399a60ff8137e6d00051c8dae71e628d4e639d
SheikhRashed <devrashed06@gmail.com>
2022-05-02 20:51:00 +0600

    update: fixed dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,29 +3,49 @@
 const body = document.querySelector("body")
 const btn = document.querySelector(".sidebar-btn")
 
-btn.addEventListener("click", (e) => {
-	e.preventDefault()
+if (btn) {
+	btn.addEventListener("click", (e) => {
+		e.preventDefault()
 
-	body.classList.toggle("open")
-})
+		body.classList.toggle("open")
+	})
+}
 
 const closeBtn = document.querySelector(".sidebar-close")
 
-closeBtn.addEventListener("click", () => {
-	body.classList.remove("open")
-})
+if (closeBtn) {
+	closeBtn.addEventListener("click", () => {
+		body.classList.remove("open")
+	})
+}
 
 window.onscroll = function () {
 	myFunction()
 }
 
 var header = document.querySelector(".header.header-primary")
-var sticky = header.offsetTop
 
-function myFunction() {
-	if (window.pageYOffset > sticky) {
-		header.classList.add("bg")
-	} else {
-		header.classList.remove("bg")
+if (header) {
+	var sticky = header.offsetTop
+
+	function myFunction() {
+		if (window.pageYOffset > sticky) {
+			header.classList.add("bg")
+		} else {
+			header.classList.remove("bg")
+		}
 	}
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+	var demo1 = new BVSelect({
+		selector: "#duration",
+		width: "100%",
+		searchbox: false,
+		offset: true,
+		placeholder: "Select Option",
+		search_placeholder: "Search...",
+		search_autofocus: true,
+		breakpoint: 450,
+	})
+})
